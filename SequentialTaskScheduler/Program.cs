ac5741da1a095e118ea3b214d63bb802ad434668
Kostiantyn CHORNYI <kostiantyn.chornyi@ctdev.io>
2018-06-21 02:07:48 +0300

    Add project files.

---
+++ SequentialTaskScheduler/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace SequentialTaskScheduler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var scheduler = new TaskScheduler();
+
+            for (int i = 0; i < 10; i++)
+            {
+                var inner = i;
+                scheduler.AddForExecution(() =>
+                {
+                    Console.WriteLine($"Exec started {inner}");
+                    Thread.Sleep(1000);
+                    Console.WriteLine($"Exec finished {inner}");
+                });
+            }
+
+            scheduler.Wait();
+            Console.WriteLine("End");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SequentialTaskScheduler/TaskScheduler.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SequentialTaskScheduler
+{
+    /// <summary>
+    /// Executes tasks sequentially in order they were added by AddForExecution method
+    /// </summary>
+    public class TaskScheduler
+    {
+        private readonly ConcurrentQueue<Action> tasks = new ConcurrentQueue<Action>();
+        private readonly List<Exception> innerExceptions = new List<Exception>();
+        private readonly object syncRoot = new object();    // Synchronization instance to create the worker
+        private volatile Task worker;  // Microsoft recommend to use volatile to ensure that assignment to the worker completes before the worker can be accessed
+
+        /// <summary>
+        /// Returns set of exceptions, that occurred during the execution of tasks
+        /// </summary>
+        public AggregateException Exception => innerExceptions.Count > 0 ? new AggregateException(innerExceptions) : null;
+
+        /// <summary>
+        /// Return current status of the worker. True, if worker is executing task, otherwise - false
+        /// </summary>
+        public bool IsBusy => worker != null && !worker.IsCompleted;
+
+        /// <summary>
+        /// Adds an action to the execution queue
+        /// </summary>
+        /// <param name="action">The instance of an action. You mustn't pass null as an action</param>
+        public void AddForExecution(Action action)
+        {
+            if (action == null) throw new ArgumentNullException(nameof(action));
+
+            tasks.Enqueue(action);
+            StartWorker();
+        }
+
+        /// <summary>
+        /// Waits for the TaskScheduler to complete execution
+        /// </summary>
+        public void Wait()
+        {
+            worker?.Wait();
+        }
+
+        /// <summary>
+        /// Starts the worker task
+        /// </summary>
+        private void StartWorker()
+        {
+            var needNewWorker = !tasks.IsEmpty && !IsBusy;
+            // Double check pattern
+            if (needNewWorker)  
+            {
+                lock (syncRoot)
+                {
+                    if (needNewWorker)
+                    {
+                        worker = Task.Factory.StartNew(WorkerMethod);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets tasks from the queue and executes them
+        /// </summary>
+        private void WorkerMethod()
+        {
+            while (!tasks.IsEmpty)
+            {
+                if (tasks.TryDequeue(out var result))
+                {
+                    try
+                    {
+                        result.Invoke();
+                    }
+                    catch (Exception ex)
+                    {
+                        innerExceptions.Add(ex);
+                    }
+                }
+            }
+        }
+    }
+}
